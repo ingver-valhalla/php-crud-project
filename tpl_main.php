0342025ad95f6db84d7435f1403c1eb69a193c07
Igor Schelkonogov <ingver.yum@gmail.com>
2017-02-11 22:05:55 +0500

    Added missed section name into page title

--- tpl_main.php
+++ tpl_main.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Предприятие</title>
+    <title><?php echo $section ?> | Предприятие</title>
   </head>
   <body>
 
