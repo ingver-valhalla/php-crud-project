947283593c576b9a12a0a617fb85197104ff6b74
Igor Schelkonogov <ingver.yum@gmail.com>
2017-02-14 20:12:57 +0500

    Added animations to links in menu

--- tpl_menu.php
+++ tpl_menu.php
@@ -1,9 +1,9 @@
 <div class="menu">
   <ul>
-		<?php foreach($menu_items as $name => $item): ?>
-			<li class="menu-item <?php if ($item->active) echo 'active' ?>">
-				<a href="<?php echo $item->link?>"><?php echo $item->name ?></a>
-			</li>
-		<?php endforeach; ?>
+    <?php foreach($menu_items as $name => $item): ?>
+      <li class="menu-item <?php if ($item->active) echo 'active' ?>">
+        <a <?php if (!$item->active) echo 'href="' . $item->link . '"' ?>"><?php echo $item->name ?></a>
+      </li>
+    <?php endforeach; ?>
   </ul>
 </div>
